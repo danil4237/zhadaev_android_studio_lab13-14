23e907aa073d7297a58b472ba972f45f9f0229ee
K1eranza <zubrevaevelina20641@gmail.com>
2026-04-25 13:41:09 +0300

    32 str

--- app/src/main/java/com/zhadaev/unscramblegame/ui_model/GameViewModel.kt
+++ app/src/main/java/com/zhadaev/unscramblegame/ui_model/GameViewModel.kt
@@ -1,11 +1,17 @@
 package com.zhadaev.unscramblegame.ui_model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.zhadaev.unscramblegame.data.GameUiState
+import com.zhadaev.unscramblegame.data.MAX_NO_OF_WORDS
+import com.zhadaev.unscramblegame.data.SCORE_INCREASE
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import com.zhadaev.unscramblegame.data.allWords
+import kotlinx.coroutines.flow.update
 
 class GameViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
@@ -13,6 +19,9 @@ class GameViewModel: ViewModel() {
 
     private lateinit var currentWord: String
     private var usedWords: MutableSet<String> = mutableSetOf()
+
+    var userGuess by mutableStateOf("")
+        private set
 
     init {
         resetGame()
@@ -43,6 +52,45 @@ class GameViewModel: ViewModel() {
         usedWords.add(currentWord)
         return shuffleCurrentWord(currentWord)
     }
+
+    fun upadteUserGuess(guessWord: String){
+        userGuess = guessWord
+    }
+
+    private fun updateGameState(updateScore: Int) {
+        if (usedWords.size == MAX_NO_OF_WORDS) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updateScore,
+                    isGameOver = true
+                )
+            }
+        } else {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentScrambleWord = pickRandomWordAndShuffle(),
+                    score = updateScore,
+                    currentWordCount = currentState.currentWordCount + 1
+                )
+            }
+        }
+    }
+
+
+    fun checkUserGuess(){
+        if (userGuess.equals(currentWord, ignoreCase = true)) {
+            val updatedScore = _uiState.value.score + SCORE_INCREASE
+            updateGameState(updatedScore)
+        } else{
+            _uiState.update { currentState ->
+                currentState.copy(isGuessedWordWrong = true)
+            }
+        }
+        updateUserGuess("") //32 страница
+    }
+
 
 
 
