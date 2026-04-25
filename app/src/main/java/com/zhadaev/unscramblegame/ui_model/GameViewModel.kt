fb045deeea31cb71737be8a5ec4edb7533eca02f
K1eranza <zubrevaevelina20641@gmail.com>
2026-04-25 11:35:09 +0300

    Step 4: Add words data and game logic

--- app/src/main/java/com/zhadaev/unscramblegame/ui_model/GameViewModel.kt
+++ app/src/main/java/com/zhadaev/unscramblegame/ui_model/GameViewModel.kt
@@ -5,16 +5,45 @@ import com.zhadaev.unscramblegame.data.GameUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import com.zhadaev.unscramblegame.data.allWords
 
 class GameViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
 
     init {
         resetGame()
     }
 
     fun resetGame(){
-
+        usedWords.clear()
+        _uiState.value = GameUiState(
+            currentScrambleWord = pickRandomWordAndShuffle()
+        )
     }
+
+    private fun shuffleCurrentWord(word: String): String{
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord) == word){
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        while (usedWords.contains(currentWord)){
+            currentWord = allWords.random()
+        }
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
+
+
+
 }
